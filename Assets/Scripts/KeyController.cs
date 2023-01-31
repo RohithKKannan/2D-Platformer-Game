10b1f3105068c265271c08c3be5048091330903c
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-01-31 09:04:18 +0530

    Key animation and fade on pickup

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    Animator keyAnimator;
+    BoxCollider2D boxCollider2D;
+    void Awake()
+    {
+        keyAnimator = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
-            Destroy(gameObject);
+            keyAnimator.SetTrigger("pickUp");
+            boxCollider2D.isTrigger = true;
         }
+    }
+    public void DestroyKey()
+    {
+        Destroy(gameObject);
     }
 }
