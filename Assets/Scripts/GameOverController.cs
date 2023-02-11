a9d8cb82034a4e49638f39ec21e89f3ff41a0c7b
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-02-12 01:23:59 +0530

    Added an Audio Manager

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -14,22 +14,28 @@ public class GameOverController : MonoBehaviour
     }
     public void GameOver()
     {
+        AudioManager.Instance.PlaySound(SoundType.GameOver);
         hud.SetActive(false);
         gameOver.SetActive(true);
         player.GetComponent<PlayerController>().enabled = false;
+        player.GetComponent<Animator>().enabled = false;
     }
     public void RestartLevel()
     {
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
+        AudioManager.Instance.PlaySound(SoundType.BGMusic);
         HeartController.hearts = 3;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void MainMenu()
     {
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
         HeartController.hearts = 3;
         SceneManager.LoadScene(0);
     }
     public void NextLevel()
     {
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
         LevelManager.Instance.LoadNextLevel();
     }
 }
--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -1,24 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 public class LobbyController : MonoBehaviour
 {
     [SerializeField] GameObject MainMenu;
     [SerializeField] GameObject Options;
     [SerializeField] GameObject LevelSelection;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider soundFxSlider;
+    void Start()
+    {
+        musicSlider.value = AudioManager.Instance.GetMusicVolume();
+        soundFxSlider.value = AudioManager.Instance.GetSfxVolume();
+    }
     public void StartGame()
     {
         LevelSelection.SetActive(true);
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
     }
     public void OpenOptions()
     {
         Options.SetActive(true);
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
     }
     public void BackToMenu()
     {
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
         Options.SetActive(false);
         LevelSelection.SetActive(false);
     }
     public void QuitGame()
     {
+        AudioManager.Instance.PlaySound(SoundType.ButtonClick);
         Application.Quit();
     }
 }
